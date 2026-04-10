0764051dd053aacc479b4aaddad940c9a47fad78
miniye2026 <al03022706@tecmilenio.mx>
2026-04-09 22:30:25 -0700

    Add files via upload

---
+++ def calculadora_suma():.py
@@ -0,0 +1,8 @@
+def calculadora_suma():
+    num1 = float(input("Ingresa el primer número: "))
+    num2 = float(input("Ingresa el segundo número: "))
+    resultado = num1 + num2
+    print("El resultado de la suma es:", resultado)
+
+if __name__ == "__main__":
+    calculadora_suma()
